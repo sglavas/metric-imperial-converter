3d467a38fade3956c9b0fd8348603c2921a7e151
sglavas <sglavas@m.ffzg.hr>
2025-10-30 23:00:20 +0100

    test: implement functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,68 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    // #1
+    test('Test GET /api/convert with 10L', (done) => {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/convert?input=10L')
+            .end((err, res) => {
+                let responseObject = JSON.parse(res.text);
+                assert.equal(res.status, 200);
+                assert.equal(responseObject.returnNum, 2.641721);
+                assert.equal(responseObject.returnUnit, 'gal');
+                done();
+            })
+    })
+    // #2
+    test('Test GET /api/convert with invalid input 32g', (done) => {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/convert?input=32g')
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'invalid unit');
+                done();
+            })
+    })
+    // #3
+    test('Test GET /api/convert with invalid number 3/7.2/4kg', (done) => {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/convert?input=3/7.2/4kg')
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'invalid number');
+                done();
+            })
+    })
+    // #4
+    test('Test GET /api/convert with invalid number and unit, 3/7.2/4kilomegagram', (done) => {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/convert?input=3/7.2/4kilomegagram')
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'invalid number and unit');
+                done();
+            })
+    })
+    // #5
+    test('Test GET /api/convert with no number, e.g. "kg"', (done) => {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/convert?input=kg')
+            .end((err, res) => {
+                let responseObject = JSON.parse(res.text);
+                assert.equal(res.status, 200);
+                assert.equal(responseObject.initNum, 1);
+                assert.equal(responseObject.returnUnit, 'lbs');
+                done();
+            })
+    })
 });
